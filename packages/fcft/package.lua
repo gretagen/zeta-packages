8b980e8e118b39b7eacce4cc01718ed6d892b5b8
throwaway-account-58577 <throwaway_account_58577@yahoo.com>
2026-08-12 17:20:09 +0100

    add foot terminal

---
+++ packages/fcft/package.lua
@@ -0,0 +1,9 @@
+return {
+  name    = "fcft",
+  version = "3.3.3",
+  summary = "Simple library for font loading and glyph rasterization",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/fcft/fcft-3.3.3.tar.gz",
+  sha256  = "a16ef6f037aa45ef0597b824db796f1b2220eb8d6152c4d11ce461475cba56e0",
+  deps    = {},
+  archive = { strip = 1 },
+}
---
+++ packages/foot/package.lua
@@ -0,0 +1,9 @@
+return {
+  name    = "foot",
+  version = "1.27.0",
+  summary = "Simple Wayland Terminal",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/foot/foot-1.27.0.tar.gz",
+  sha256  = "06239b0baf90ef8f504f421f5ff404f94953797d8138a9be6ad568deb6ac332b",
+  deps    = {},
+  archive = { strip = 1 },
+}
