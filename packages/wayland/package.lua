28dc1e384d37765b735efdb282cedf851b2d70e5
gretagen <besternet91@gmail.com>
2026-08-24 19:42:06 +0200

    fix: wayland tarball double-nesting (files landed at /wayland-1.26.0/usr/ instead of /usr/)

--- packages/wayland/package.lua
+++ packages/wayland/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.26.0",
   summary = "Wayland compositor infrastructure: client/server libraries and scanner",
   url = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/wayland/wayland-1.26.0.tar.gz",
-  sha256 = "4f2cf466ebd8dec327928ec405bd9d5a0cec05758e97214d4f29f3fed229550c",
+  sha256 = "bb141f26535d9076f76bf97c93cdedfaeadd0b849aa0a7471062dd3ff4e29766",
   deps = { "libffi", "libxml2" },
   archive = { strip = 1 },
   test = function(p)
