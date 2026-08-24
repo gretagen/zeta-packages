353af23e46a04b90ecb9bd231ccce21f6894b1a6
gretagen <besternet91@gmail.com>
2026-08-24 19:13:41 +0200

    feat: wayland - add XDG_RUNTIME_DIR profile.d script for Wayland compositors

--- packages/wayland/package.lua
+++ packages/wayland/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.26.0",
   summary = "Wayland compositor infrastructure: client/server libraries and scanner",
   url = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/wayland/wayland-1.26.0.tar.gz",
-  sha256 = "da7f38b3482ace5420ce283001c6172db2a8e3020a7792c8c27b3c740da38e16",
+  sha256 = "4f2cf466ebd8dec327928ec405bd9d5a0cec05758e97214d4f29f3fed229550c",
   deps = { "libffi", "libxml2" },
   archive = { strip = 1 },
   test = function(p)
