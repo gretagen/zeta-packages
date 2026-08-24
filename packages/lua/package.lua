04c3fcd61b03b59eb1b8bb080356735c4eef1762
gretagen <besternet91@gmail.com>
2026-08-24 17:59:43 +0200

    fix: lua package URL to use commit SHA bypassing CDN cache

--- packages/lua/package.lua
+++ packages/lua/package.lua
@@ -2,7 +2,7 @@ return {
   name    = "lua",
   version = "5.5.1",
   summary = "Lua 5.5 shared library (liblua) for embedding",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/lua/lua-5.5.1.tar.gz",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/453bbab88e63cc05a7378f793217baf90ca3707c/packages/lua/lua-5.5.1.tar.gz",
   sha256  = "bba0351ca927c7712a3abfe075fcea8c6043c8ac49201f665809ad9b58d8cfa2",
   deps    = {},
   archive = { strip = 1 },
