5b59ff39dd21982062c36b3ae7c56b32c7fc49d6
gretagen <besternet91@gmail.com>
2026-08-25 18:14:26 +0200

    fix: mesa-drivers - add GLVND EGL vendor registration (50_mesa.json)

--- packages/mesa-drivers/package.lua
+++ packages/mesa-drivers/package.lua
@@ -3,10 +3,11 @@ return {
   version = "26.1.8",
   summary = "Mesa 3D drivers (radeonsi, i915, lavapipe, etc.)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/mesa-drivers/mesa-drivers-26.1.8.tar.gz",
-  sha256  = "9aa0f46caa64a6aa648096481d4e3a7a4909b920d926dcbceb03993f97082932",
+  sha256  = "519746efd339c88764a6eb6395ce37316745e876109ec3c1db48670c232c228b",
   deps    = { "llvm-libs", "libglvnd", "libX11", "libxcb", "wayland", "libpciaccess", "libXxf86vm", "libigdgmm" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -f " .. p.install_root .. "/usr/lib/dri/radeonsi_dri.so")
+    p:run("test -f " .. p.install_root .. "/usr/share/glvnd/egl_vendor.d/50_mesa.json")
   end,
 }
