672f7b2e270887f7e651e9cf41518eddfc3e01e1
gretagen <besternet91@gmail.com>
2026-08-26 19:25:29 +0200

    Remove non-existent db dep from iproute2

--- packages/iproute2/package.lua
+++ packages/iproute2/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Advanced IP routing and network tools (ip, ss, tc, bridge, devlink)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/iproute2/iproute2-7.2.0.tar.gz",
   sha256  = "5ffcb0b5db93f95e1d3989f45fdc475a38882fc2e50c8e22cef15112d0fbf691",
-  deps    = { "libz", "db" },
+  deps    = { "libz" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/sbin/ip && test -x " .. p.install_root .. "/sbin/ss && test -x " .. p.install_root .. "/sbin/tc")
