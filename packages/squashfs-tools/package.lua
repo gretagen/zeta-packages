2b8056e295cd30d66e24ff1604abe7d347edfb7e
gretagen <besternet91@gmail.com>
2026-08-26 17:20:59 +0200

    Fix squashfs-tools dep: xz -> xz-utils

--- packages/squashfs-tools/package.lua
+++ packages/squashfs-tools/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Tools for creating and extracting squashfs filesystems (mksquashfs, unsquashfs)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/squashfs-tools/squashfs-tools-4.7.5.tar.gz",
   sha256  = "e4ae769184a74b6ed9c75d5cc3b2dab6e606904a5503d3210ee98f6036ba0c84",
-  deps    = { "libz", "lzo", "lz4", "libzstd", "xz" },
+  deps    = { "libz", "lzo", "lz4", "libzstd", "xz-utils" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/mksquashfs && test -x " .. p.install_root .. "/usr/bin/unsquashfs")
