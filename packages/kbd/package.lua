4c5d7aee79d27008e585f700478cafcd2f9f559a
gretagen <besternet91@gmail.com>
2026-08-26 08:23:34 +0200

    Add lzo 2.10 and kbd 2.10.0

---
+++ packages/kbd/package.lua
@@ -0,0 +1,12 @@
+return {
+  name    = "kbd",
+  version = "2.10.0",
+  summary = "Keyboard and console utilities (loadkeys, setfont, dumpkeys, showkey, etc.)",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/kbd/kbd-2.10.0.tar.gz",
+  sha256  = "65fc73eed4f6f18a0390ef1dc4ea4405a157c01f81b411c6501203afb75481f6",
+  deps    = {},
+  archive = { strip = 1 },
+  test    = function(p)
+    p:run("test -x " .. p.install_root .. "/usr/bin/loadkeys && test -x " .. p.install_root .. "/usr/bin/setfont")
+  end,
+}
