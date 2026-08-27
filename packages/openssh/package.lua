ba9fe2976b9c54497b2fb577f9a3235b67691027
gretagen <besternet91@gmail.com>
2026-08-27 05:13:31 +0200

    fix: rebuild openssh as binary package (3.1 MiB)

--- packages/openssh/package.lua
+++ packages/openssh/package.lua
@@ -3,11 +3,10 @@ return {
   version = "10.3p1",
   summary = "OpenSSH protocol implementation for remote login, command execution and file transfer",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/openssh/openssh-10.3p1.tar.gz",
-  sha256  = "56682a36bb92dcf4b4f016fd8ec8e74059b79a8de25c15d670d731e7d18e45f4",
-  deps    = { "openssl", "pam", "libz" },
+  sha256  = "50708ca0892531e9e074a47782e8f533954a2aadfdd30475d868be812790b34e",
+  deps    = { "openssl", "pam", "libz", "libedit" },
   archive = { strip = 1 },
   test    = function(p)
-    p:run("test -f " .. p.install_root .. "/usr/bin/sshd")
-    p:run("test -f " .. p.install_root .. "/usr/bin/ssh")
+    p:run("test -f " .. p.install_root .. "/usr/bin/ssh && test -f " .. p.install_root .. "/usr/sbin/sshd")
   end,
 }
