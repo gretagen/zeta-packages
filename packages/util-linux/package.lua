f8e7ed1b05618e80b59189ebd6baf3d4e63ab1ae
huntedbytheirs <huntedbyth3irs@gmail.com>
2026-08-27 17:26:02 -0400

    feat: rebuild util-linux 2.42 from source (full toolset, setuid mount/su/umount)

--- packages/util-linux/package.lua
+++ packages/util-linux/package.lua
@@ -1,12 +1,12 @@
 return {
   name    = "util-linux",
   version = "2.42",
-  summary = "Essential Linux system utilities (mount, losetup, fdisk, blkid, hwclock, agetty, etc.)",
+  summary = "Essential Linux system utilities (mount, losetup, fdisk, blkid, agetty, lsblk, etc.)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/util-linux/util-linux-2.42.tar.gz",
-  sha256  = "2ad909a915013fe30662f2631c85bc4c8a241bf152b8922004103cba69083a68",
-  deps    = { "glibc", "systemd-libs", "pam", "libz" },
+  sha256  = "f194bf3167f4ee7f00e289bbfe42f9793b6baeabe225d6bd34655f657e2b8b7b",
+  deps    = { "glibc", "systemd-libs", "pam", "libz", "gcc-libs" },
   archive = { strip = 1 },
   test    = function(p)
-    p:run("test -x " .. p.install_root .. "/usr/bin/mount && test -x " .. p.install_root .. "/usr/sbin/losetup && test -x " .. p.install_root .. "/usr/sbin/agetty")
+    p:run("test -x " .. p.install_root .. "/usr/bin/mount -a -x " .. p.install_root .. "/usr/sbin/agetty -a -x " .. p.install_root .. "/usr/bin/lsblk")
   end,
 }
