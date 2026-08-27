e753520950b7484bd3c44f3f71c11b70208b815d
huntedbytheirs <huntedbyth3irs@gmail.com>
2026-08-27 18:09:14 -0400

    fix: systemd-libs missing libudev (breaks dhcpcd, grub, device-mapper)

--- packages/systemd-libs/package.lua
+++ packages/systemd-libs/package.lua
@@ -3,7 +3,7 @@ return {
   version = "257",
   summary = "systemd libraries (libsystemd, libudev)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/systemd-libs/systemd-libs-257.tar.gz",
-  sha256  = "66252eac75bb74fd77aefe0a86c1c6e742f2aac63ee9d1689cc6104e0db54d9d",
+  sha256  = "7cb3f65300754e012e768c7fbcffda9ca18e3ab139d71a9454e600c93ad83dd8",
   deps    = { "glibc" },
   archive = { strip = 1 },
   test    = function(p)
