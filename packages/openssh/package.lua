5e152b3f8d31767568993995683c65394eb8ba75
huntedbytheirs <huntedbyth3irs@gmail.com>
2026-08-27 18:00:03 -0400

    fix: openssh missing libmd dep (ssh-keygen fails at runtime)

--- packages/openssh/package.lua
+++ packages/openssh/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "OpenSSH protocol implementation for remote login, command execution and file transfer",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/openssh/openssh-10.3p1.tar.gz",
   sha256  = "50708ca0892531e9e074a47782e8f533954a2aadfdd30475d868be812790b34e",
-  deps    = { "glibc", "openssl", "pam", "libz", "libedit" },
+  deps    = { "glibc", "openssl", "pam", "libz", "libedit", "libmd" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -f " .. p.install_root .. "/usr/bin/ssh && test -f " .. p.install_root .. "/usr/sbin/sshd")
