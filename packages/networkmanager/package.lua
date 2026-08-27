c328e202c6da345224297f9384ec3ffa1aa7eb62
gretagen <besternet91@gmail.com>
2026-08-27 03:23:29 +0200

    fix: networkmanager dep zlib -> libz

--- packages/networkmanager/package.lua
+++ packages/networkmanager/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Network management daemon (wifi iwd+wpa_supplicant, dnsmasq, dhcpcd)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/networkmanager/networkmanager-1.51.4.tar.gz",
   sha256  = "1a15edef4ba3467af405867fe2619b1827d96e372c88b44d2b88f03ae20465be",
-  deps    = { "glib", "dbus", "libnl", "curl", "libndp", "libpsl", "systemd-libs", "zlib", "iwd", "wpa_supplicant", "dnsmasq", "dhcpcd", "gobject-introspection", "polkit", "elogind", "libgudev", "ncurses", "nss" },
+  deps    = { "glib", "dbus", "libnl", "curl", "libndp", "libpsl", "systemd-libs", "libz", "iwd", "wpa_supplicant", "dnsmasq", "dhcpcd", "gobject-introspection", "polkit", "elogind", "libgudev", "ncurses", "nss" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -f " .. p.install_root .. "/usr/sbin/NetworkManager")
