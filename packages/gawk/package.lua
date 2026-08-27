e044ffbe7d966f78eb3ea16fb11df16c75b902c0
gretagen <besternet91@gmail.com>
2026-08-27 03:22:22 +0200

    fix: add mpfr dependency to gawk

--- packages/gawk/package.lua
+++ packages/gawk/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "GNU awk - pattern scanning and processing language",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/gawk/gawk-5.4.1.tar.gz",
   sha256  = "bf482155ac29fdf49ed65f17553aa6ec69329f450ced7dc8d79458a828d039f2",
-  deps    = {},
+  deps    = { "mpfr" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/gawk && test -L " .. p.install_root .. "/usr/bin/awk")
