53a0ea8c22fe3f797bce7565994a00747ada157b
gretagen <besternet91@gmail.com>
2026-09-06 01:22:58 +0200

    iptables: fix absolute symlink usr/bin/iptables-xml -> /usr/sbin/xtables-legacy-multi
    Upstream install-data-hook links iptables-xml with the absolute sbindir
    path; Zeta's unpack rejects absolute symlink targets as root escapes.
    Relink relatively (../sbin/xtables-legacy-multi) at package time via the
    new post_install recipe hook.

--- packages/iptables/package.lua
+++ packages/iptables/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.8.13",
   summary = "Packet filtering and NAT toolset (iptables/ip6tables, libxtables)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/iptables/iptables-1.8.13.tar.gz",
-  sha256  = "ace405b2b67d73036692ca5bc6a4038851a0ecd0cfd8ebefa61e15c092ef3695",
+  sha256  = "73aaa6e68127c79d1ab4df54df39d874b0889987938a7e5bb8afe493f8f52dc1",
   deps    = { "glibc" },
   archive = { strip = 1 },
   test    = function(p)
