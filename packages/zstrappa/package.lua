26e30981feaeff8331a23a55ffd6ad02be138d2e
gretagen <besternet91@gmail.com>
2026-09-06 11:18:04 +0200

    zstrappa: rebrand to Heliade OS (etc/heliade paths, boot labels, prompts)

--- packages/zstrappa/package.lua
+++ packages/zstrappa/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zstrappa",
   version = "1.0",
-  summary = "Install Zerene OS from the live ISO to a target device",
+  summary = "Install Heliade OS from the live ISO to a target device",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.0.tar.gz",
-  sha256  = "0f1ca384a386d32eadeb239373cbcabff67497bff005c15699c7084ca7a95393",
+  sha256  = "bbbbf97c31bd7bfc2af65726049cb0574036f190277097594993ad6459e506e8",
   deps    = { "bash", "btrfs-progs", "util-linux", "dosfstools", "limine", "rsync" },
   archive = { strip = 1 },
   test    = function(p)
