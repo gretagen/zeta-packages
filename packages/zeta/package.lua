bba6f34bd4674a94c8c0147085ac39c779f57d45
gretagen <besternet91@gmail.com>
2026-09-09 20:22:46 +0200

    zeta: repackage with parallel downloads

--- packages/zeta/package.lua
+++ packages/zeta/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zeta",
   version = "1.0.6",
-  summary = "Zeta package manager for Heliade OS",
+  summary = "Zeta package manager for Haliade OS",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.6.tar.gz",
-  sha256  = "012ea1205e23442c59602127a861175b5cf25a7dd3b7e3ec38d27d4222bf7ccc",
+  sha256  = "055e84182936def706308dc1b50dfc7144a1b28fc0a73a0a9556cd523c1b14e6",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
