8a8348b99a25bf3788a1f9405056fdddd588764f
gretagen <besternet91@gmail.com>
2026-09-10 20:28:38 +0200

    zstrappa 2.0: rewrite — no-chroot initramfs gen, clean fstab, fix fstab.lua

--- packages/zstrappa/package.lua
+++ packages/zstrappa/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zstrappa",
-  version = "1.5",
+  version = "2.0",
   summary = "Install Haliade OS from the live ISO to a target device",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.5.tar.gz",
-  sha256  = "e4c496d73f9385f39e5cfc87eeda055816736acc04e6c8708e92d4ab280c8988",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-2.0.tar.gz",
+  sha256  = "fef97dad9c45e11feb31f035f911f94f5e68e7a64b29f2cf32678319130c3057",
   deps    = { "bash", "btrfs-progs", "util-linux", "dosfstools", "limine", "rsync" },
   archive = { strip = 1 },
   test    = function(p)
