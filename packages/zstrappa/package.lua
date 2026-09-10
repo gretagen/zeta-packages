f881fc3498fec5cbe50eecd13067ce26d0f272e4
gretagen <besternet91@gmail.com>
2026-09-10 18:59:41 +0200

    zstrappa 1.4: create installed.preset with detected kver for mkinitcpio

--- packages/zstrappa/package.lua
+++ packages/zstrappa/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zstrappa",
-  version = "1.3",
+  version = "1.4",
   summary = "Install Haliade OS from the live ISO to a target device",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.3.tar.gz",
-  sha256  = "b1588f5b786adb26a71b7a92301f23c887a3e9abb383371c1705fdaa22ddda30",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.4.tar.gz",
+  sha256  = "2457adf258a893b1c2e26f304cee11f2b8fffe970aa63cf0e0dad0fe2cedb084",
   deps    = { "bash", "btrfs-progs", "util-linux", "dosfstools", "limine", "rsync" },
   archive = { strip = 1 },
   test    = function(p)
