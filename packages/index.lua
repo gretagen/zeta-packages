40155b7c3404ceb0d8d6c15a8faf23327eed6e4a
gretagen <besternet91@gmail.com>
2026-09-10 08:07:38 +0200

    zstrappa: 1.1

--- packages/index.lua
+++ packages/index.lua
@@ -544,5 +544,5 @@ return {
   { name = "zip", version = "3.0", summary = "InfoZIP archiver" },
   { name = "zsh", version = "5.9.2", summary = "Powerful shell with scripting and tab completion" },
   { name = "zstd", version = "1.5.7", summary = "Zstandard compression library" },
-  { name = "zstrappa", version = "1.0", summary = "Install Haliade OS from the live ISO to a target device" },
+  { name = "zstrappa", version = "1.1", summary = "Install Haliade OS from the live ISO to a target device" },
 }
--- packages/zstrappa/package.lua
+++ packages/zstrappa/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zstrappa",
-  version = "1.0",
-  summary = "Install Heliade OS from the live ISO to a target device",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.0.tar.gz",
-  sha256  = "54668f57f3b8a817ae6fd58f42d44e569678f1d060c21e436fc525bed7e11a59",
+  version = "1.1",
+  summary = "Install Haliade OS from the live ISO to a target device",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.1.tar.gz",
+  sha256  = "090ec3a5e2151ddf22618f5ee222314ed917d606918e5b8e9d0e9b7b2b51c45f",
   deps    = { "bash", "btrfs-progs", "util-linux", "dosfstools", "limine", "rsync" },
   archive = { strip = 1 },
   test    = function(p)
