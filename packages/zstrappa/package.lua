d33de388a1f4e848568d2ac5b77c8a249462e815
gretagen <besternet91@gmail.com>
2026-09-10 18:43:02 +0200

    zstrappa 1.3: fix boot order, iwd setuid, expand dirs, robust initramfs gen

--- packages/zstrappa/package.lua
+++ packages/zstrappa/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zstrappa",
-  version = "1.2",
+  version = "1.3",
   summary = "Install Haliade OS from the live ISO to a target device",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.2.tar.gz",
-  sha256  = "1ecfe9b1c8d93c487af79571360305b87b5fc28da6a1c70f748d150e119a2b19",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-1.3.tar.gz",
+  sha256  = "b1588f5b786adb26a71b7a92301f23c887a3e9abb383371c1705fdaa22ddda30",
   deps    = { "bash", "btrfs-progs", "util-linux", "dosfstools", "limine", "rsync" },
   archive = { strip = 1 },
   test    = function(p)
