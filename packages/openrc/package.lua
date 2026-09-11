4166a2174756d1956ede5996651599cc12bd0b08
gretagen <besternet91@gmail.com>
2026-09-11 16:22:56 +0200

    openrc: fix runlevel symlinks (absolute → relative for strip=1)

--- packages/openrc/package.lua
+++ packages/openrc/package.lua
@@ -3,7 +3,7 @@ return {
   version = "0.63.3",
   summary = "OpenRC init system and service manager for Haliade OS",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/openrc/openrc-0.63.3.tar.gz",
-  sha256  = "84cd82cfdce2e4518af58ab83606bdc6916ec9448ccc06f3dbb399a7c2bb5cfc",
+  sha256  = "dd2364739ddd12f046ce022cab9812fa0b2f36863ccf2775e9f104caacc14f54",
   deps    = { "glibc", "pam", "libcap", "libcap-ng", "audit" },
   archive = { strip = 1 },
   test    = function(p)
