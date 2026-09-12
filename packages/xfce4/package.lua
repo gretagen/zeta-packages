963db84b885f49db8be377043bea3faf03bfc033
gretagen <besternet91@gmail.com>
2026-09-12 12:32:11 +0200

    xfce4: add startup-notification dependency (libstartup-notification-1.so.0)

--- packages/xfce4/package.lua
+++ packages/xfce4/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Xfce desktop group (meta-package)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/xfce4/xfce4-4.20.tar.gz",
   sha256  = "7d7587af5dc6cd4f8eb9d3ef33af82d77faa89466c03493330e52688d6d4ca48",
-  deps    = { "libxfce4util", "xfconf", "garcon", "libxfce4ui", "exo", "libxfce4windowing", "xfce4-session", "xfce4-panel", "xfce4-settings", "xfce4-appfinder", "xfwm4", "xfdesktop", "thunar", "xfce4-terminal", "xfce4-power-manager", "xfce4-notifyd", "libwnck3", "libxklavier", "vte", "libnotify", "upower", "libXres", "libXpresent", "libdisplay-info", "sqlite", "tango-icon-theme", "adwaita-icon-theme" },
+  deps    = { "libxfce4util", "xfconf", "garcon", "libxfce4ui", "exo", "libxfce4windowing", "xfce4-session", "xfce4-panel", "xfce4-settings", "xfce4-appfinder", "xfwm4", "xfdesktop", "thunar", "xfce4-terminal", "xfce4-power-manager", "xfce4-notifyd", "libwnck3", "libxklavier", "vte", "libnotify", "upower", "libXres", "libXpresent", "libdisplay-info", "sqlite", "tango-icon-theme", "adwaita-icon-theme", "startup-notification" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -d " .. p.install_root)
