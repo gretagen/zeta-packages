109d7b150fd3d9974060be0d06d039f2b563ec67
gretagen <besternet91@gmail.com>
2026-09-12 12:44:57 +0200

    cairo: rebuild with Xlib/XCB/Xrender support (fix cairo_xlib_surface_get_display)

--- packages/cairo/package.lua
+++ packages/cairo/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.18.4",
   summary = "2D graphics library with Xlib/XCB surfaces",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/cairo/cairo-1.18.4.tar.gz",
-  sha256  = "6aa01723f521a35df4e7f0a7d46fe72671fe845a01c6d7214349282a850c6a7f",
-  deps    = { "glibc" },
+  sha256  = "ee7f2297c5b597adb95d8157dfd1286b2d681d123164e5d9504d7c3918a61009",
+  deps    = { "glibc", "libX11", "libXext", "libXrender", "libxcb", "pixman" },
   archive = { strip = 1 },
 }
