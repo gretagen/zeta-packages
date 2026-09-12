3e142033853db110dd0c3bad031458fac86ef0ff
gretagen <besternet91@gmail.com>
2026-09-12 13:38:07 +0200

    discord: use full raw URL for remote installs

--- packages/discord/package.lua
+++ packages/discord/package.lua
@@ -2,7 +2,7 @@ return {
   name    = "discord",
   version = "1.0.157",
   summary = "Discord (stable Electron client)",
-  url     = "discord-1.0.157.deb",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/discord/discord-1.0.157.deb",
   sha256  = "ed8773b0b85838dc8e18ec2317ac55bdc6fc7b6af0cc46109ceba002764ffbaa",
   deps    = { "glibc", "alsa-lib", "at-spi2-core", "cairo", "expat", "glib", "gtk3", "libX11", "libXcomposite", "libXdamage", "libXext", "libXfixes", "libXrandr", "libXrender", "libxss", "libXtst", "libxcb", "mesa-drivers", "nss", "pango", "pulseaudio", "wayland", "xkbcommon" },
   archive = { strip = 1 },
