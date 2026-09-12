f351d33e4dfebca14630894cfa0c1c22abd9e919
gretagen <besternet91@gmail.com>
2026-09-12 12:50:42 +0200

    zstrappa: repackage with updated script

--- packages/zstrappa/package.lua
+++ packages/zstrappa/package.lua
@@ -3,7 +3,7 @@ return {
   version = "2.4",
   summary = "Install Haliade OS from the live ISO to a target device",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zstrappa/zstrappa-2.4.tar.gz",
-  sha256  = "da0005fe9583823e6bbe3d6ef9c2a66e728cb700fc5c5cd0d5cc9bee01985828",
+  sha256  = "12a82a405ab626da5e567cc52ef97eaa2545b0bbc69acfe6bb02e1faabb3dd2b",
   deps    = { "bash", "btrfs-progs", "util-linux", "dosfstools", "limine", "rsync" },
   archive = { strip = 1 },
   test    = function(p)
